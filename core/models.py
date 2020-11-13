e6d97424e57fc7d0e860734afcec3265a8d6b2d1
Paulyne <paulynewambui2020@gmail.com>
2020-11-14 01:03:55 +0300

    add the category model

--- core/models.py
+++ core/models.py
@@ -3,14 +3,20 @@ from django.db import models
 # Create your models here.
 #images model 
 class Images(models.Model):
-    
     name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='cars')
+    image = models.ImageField(upload_to='images')
     description = models.TextField()
     location = models.CharField()
     category = models.CharField()
     
   
-class Location():
+class Location(models.Model):
     name = models.CharField()
+    description = models.TextField()
+    Images = models.ForeignKey(Images,on_delete=models.CASCADE,)
+    
+class Category(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    Images = models.ForeignKey(Images,on_delete=models.CASCADE,)
     
