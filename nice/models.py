138d473c802f03b685916f805aa2ea3f33e55832
Paulyne <paulynewambui2020@gmail.com>
2020-11-15 02:38:23 +0300

    created our three models

--- nice/models.py
+++ nice/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Images(models.Model):
+    image = models.ImageField(upload_to='images')
+    image_name = models.CharField(max_length =30)
+    description = models.TextField()
+    location = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    
+
+class Location(models.Model):
+   
+
+class Category(models.Model):
+   
