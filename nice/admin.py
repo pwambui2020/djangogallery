8368314cee9fd9ab1f8719161a1f2232d08a688b
Paulyne <paulynewambui2020@gmail.com>
2020-11-15 19:31:40 +0300

    added admin dashboard

--- nice/admin.py
+++ nice/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Images, Location,Category
 # Register your models here.
+# class ImageAdmin(admin.ModelAdmin):
+#     filter_horizontal =('images',)
+
+admin.site.register(Images)
+admin.site.register(Location)
+admin.site.register(Category)
