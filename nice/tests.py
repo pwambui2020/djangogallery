695cc07f6253a52d79fd315abd4082b58e1d7ed7
Paulyne <paulynewambui2020@gmail.com>
2020-11-15 03:28:57 +0300

    ran an image test class

--- nice/tests.py
+++ nice/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Images,Category,Location
 
 # Create your tests here.
+class ImagesTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.image = Images(image='image', image_name='', description='', location='', category='')
+        
+# Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Images))
