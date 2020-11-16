bf6cb4bdfe0fd194ff889b5791f870573a0d204f
Paulyne <paulynewambui2020@gmail.com>
2020-11-16 17:31:18 +0300

    connected the search results to the category

--- nice/urls.py
+++ nice/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path,re_path
 from . import views
+
 
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
-    path('category',views.category,name='category'),
-    path('location',views.location,name='location')
+    path('category/',views.search_results,name='category'),
+    path('location',views.location,name='location'),
+    # re_path('search', views.search_image, name='search_results'),
+    
+    
     
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
