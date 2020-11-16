a6b1250533f68790c6d196a4c8c148fff87a2c72
Paulyne <paulynewambui2020@gmail.com>
2020-11-16 12:40:47 +0300

    add a save function

--- nice/models.py
+++ nice/models.py
@@ -1,19 +1,33 @@
 from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
 
 class Images(models.Model):
-    image = models.ImageField(upload_to='images')
+    # image = models.ImageField(upload_to='images/')
     image_name = models.CharField(max_length =30)
     description = models.TextField()
     location = models.CharField(max_length=50)
     category = models.CharField(max_length=50)
+    image = CloudinaryField('image')
     
     def __str__(self):
-        return self.image
+        return self.image_name
+    
+    def save_image(self):
+        self.save()
     
     class Meta:
         ordering = ['image']
+        
+    
+    @classmethod
+    def search_by_category(cls, category):
+        images = Images.objects.filter(category__name__icontains=category)
+        return images
+   
     
 
 class Location(models.Model):
@@ -26,4 +40,7 @@ class Category(models.Model):
     name = models.CharField(max_length =30)
     def __str__(self):
         return self.name
-   
+    
+
+    
+    
