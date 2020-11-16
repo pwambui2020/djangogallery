341d5c950d68adc31f48e8409c89ec98ed3efb55
Paulyne <paulynewambui2020@gmail.com>
2020-11-16 07:34:06 +0300

    add cloudinary

--- gallery/urls.py
+++ gallery/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+# from images import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('nice.urls'))
-    # path('', category.as_view(), name='category')
-    
-]
+    path('',include('nice.urls')),
+    # path('',views.welcome, name='welcome')
+   
+  ]
